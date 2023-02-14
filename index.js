a55744974d87af8e0c7b94b86c53e8c7752116f8
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-02-14 23:42:04 +0100

    commit 23:42

---
+++ index.js
@@ -0,0 +1,37 @@
+require("dotenv").config(); // Permet d'activer les variables d'environnement qui se trouvent dans le fichier `.env`
+const express = require("express"); //J'importe express  I
+const cors = require("cors"); //Pour autoriser ou non les demandes d'autres sites d'utiliser les ressources
+const mongoose = require("mongoose"); //J'importe mongoose  II
+const app = express(); //Je créé mon serveur III
+app.use(express.json()); //On veut que nos routes récupèrent les body  IV
+// mongoose.set("strictQuery", false);
+// mongoose.connect(process.env.MONGODB_URI); //Je me connecte à ma bdd Mongo V *****edit pour exo du front vinted en react, je commente mnogoose car on va utiliser une api****
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+const uid2 = require("uid2");
+const userRoutes = require("./routes/user"); //Import de mes routes
+const offerRoutes = require("./routes/offer");
+const cloudinary = require("cloudinary").v2; //On met bien le v2 comme mentionné dans la doc
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+app.use(userRoutes);
+app.use(offerRoutes);
+app.use(cors());
+
+app.post("/", async (req, res) => {
+  res.status(200).json({ message: "TEST requete Http" });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Error 404 not found" });
+});
+
+app.listen(process.env.PORT, () => {
+  //Je fais tourner mon serveur sur le port 3000 VI
+  console.log("👕 Server started 👕");
+});
